134f885bc78e35bb1f9f2a8c61c1792db30435fa
Alex Volkov <alex@flamy.ca>
2015-11-14 12:08:46 -0500

    Collecting xml events.

--- read_xml_lxml.py
+++ read_xml_lxml.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
+## Collecting xml element tree events with  O(1) memory usage.
 from xml.etree import cElementTree
-
-events = {}
 
 
 def collect_events(fname):
     out_events = {}
     for event, elem in cElementTree.iterparse('tpl.xml'):
-        if event in events:
-            events[event] += 1
+        if event in out_events:
+            out_events[event] += 1
         else:
-            events[event] = 1
+            out_events[event] = 1
         elem.clear()
     return out_events
 
 if __name__ == "__main__":
     all_events = collect_events('tpl.xml')
+    import ipdb; ipdb.set_trace()
