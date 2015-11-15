3462faf539ce35e27e67d04c3be41663464be566
Alex Volkov <alex@flamy.ca>
2015-11-15 12:44:02 -0500

    Counting keys for presentation.

--- tpl_key_freq.py
+++ tpl_key_freq.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
 import json
+from pprint import pprint
+from collections import OrderedDict
 
 key_freq = json.loads(open('key_stats.json', 'r').read())
 
-sorted_keys = sorted([ x for x in key_freq.items()], key=lambda x: x[1])
+sorted_keys = sorted([x for x in key_freq.items()], key=lambda x: x[1])
 
-most_freq = sorted_keys[-10:]
-least_frew = sorted_keys[:10]
+most_freq = OrderedDict([x for x in reversed(sorted_keys[-11:])])
+least_freq = OrderedDict(sorted_keys[:10])
 
-import ipdb; ipdb.set_trace()
+print("10 most frequent keys:")
+pprint(most_freq)
+print()
+print("10 least frequent:")
+pprint(least_freq)
